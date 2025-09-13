ee13839a887c19651a4b5e3d306d46c055df6b3b
iAmSomeBoy <ashikur2401@gmail.com>
2025-09-13 21:09:40 +0600

    added

---
+++ polymorphism.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Parent{
+    public:
+        virtual void getInfo(){
+            cout<<"Parent class"<<endl;
+        }
+};
+
+class Child: public Parent{
+    public:
+        void getInfo(){
+            cout<<"Child Class"<<endl;
+        }
+};
+
+int main(){
+
+    Parent one;
+    one.getInfo();
+
+    Child two;
+    two.getInfo();
+
+    return 0;
+}

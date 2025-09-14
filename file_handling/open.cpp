358366e6e48b5f442773eeca8581b2fcad8c0f46
iAmSomeBoy <ashikur2401@gmail.com>
2025-09-15 02:53:11 +0600

    open() and close() functions

--- file_handling/open.cpp
+++ file_handling/open.cpp
@@ -7,6 +7,14 @@ int main(){
     fstream file1;
 
     file1.open("file1.txt",ios:: out);
+    if(!file1){
+        cout<<"Error whiling creating the file"<<endl;
+    }
+    else{
+        cout<<"Files are created and got written"<<endl;
+        file1<<"This text is being pushed into the file";
+        file1.close();
+    }
 
     
     return 0;
